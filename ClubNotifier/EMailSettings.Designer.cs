509bf2a7aa9812d506ab53a7c698ab6cb719c1c6
Chan, Danny <danny50610@yahoo.com.tw>
2015-07-14 16:43:55 +0800

    Add 'don't save password check box' in e-mail settings form

--- ClubNotifier/EMailSettings.Designer.cs
+++ ClubNotifier/EMailSettings.Designer.cs
@@ -24,24 +24,25 @@
         /// </summary>
         private void InitializeComponent() {
             this.userInfoGroupBox = new System.Windows.Forms.GroupBox();
-            this.mailLabel = new System.Windows.Forms.Label();
-            this.mailTextBox = new System.Windows.Forms.TextBox();
             this.userNameLabel = new System.Windows.Forms.Label();
             this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.mailLabel = new System.Windows.Forms.Label();
+            this.mailTextBox = new System.Windows.Forms.TextBox();
             this.serverInfoGroupBox = new System.Windows.Forms.GroupBox();
-            this.enableSSLCheckBox = new System.Windows.Forms.CheckBox();
+            this.SmtpServerLabel = new System.Windows.Forms.Label();
+            this.SmtpServerTextBox = new System.Windows.Forms.TextBox();
             this.serverPortLabel = new System.Windows.Forms.Label();
             this.serverPortTextBox = new System.Windows.Forms.TextBox();
-            this.SmtpServerTextBox = new System.Windows.Forms.TextBox();
-            this.SmtpServerLabel = new System.Windows.Forms.Label();
+            this.enableSSLCheckBox = new System.Windows.Forms.CheckBox();
             this.loginInfoGroupBox = new System.Windows.Forms.GroupBox();
-            this.loginPasseordLabel = new System.Windows.Forms.Label();
             this.loginUsernameLabel = new System.Windows.Forms.Label();
-            this.loginPasseordTextBox = new System.Windows.Forms.TextBox();
             this.loginUsernameTextBox = new System.Windows.Forms.TextBox();
+            this.loginPasseordLabel = new System.Windows.Forms.Label();
+            this.loginPasseordTextBox = new System.Windows.Forms.TextBox();
             this.OKButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.sendTestMailButton = new System.Windows.Forms.Button();
+            this.noSavePasswordCheckBox = new System.Windows.Forms.CheckBox();
             this.userInfoGroupBox.SuspendLayout();
             this.serverInfoGroupBox.SuspendLayout();
             this.loginInfoGroupBox.SuspendLayout();
@@ -61,26 +62,6 @@
             this.userInfoGroupBox.TabIndex = 0;
             this.userInfoGroupBox.TabStop = false;
             this.userInfoGroupBox.Text = "使用者資訊";
-            // 
-            // mailLabel
-            // 
-            this.mailLabel.AutoSize = true;
-            this.mailLabel.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.mailLabel.Location = new System.Drawing.Point(6, 57);
-            this.mailLabel.Name = "mailLabel";
-            this.mailLabel.Size = new System.Drawing.Size(99, 17);
-            this.mailLabel.TabIndex = 15;
-            this.mailLabel.Text = "電子郵件地址：";
-            // 
-            // mailTextBox
-            // 
-            this.mailTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.mailTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.mailTextBox.Location = new System.Drawing.Point(141, 54);
-            this.mailTextBox.Name = "mailTextBox";
-            this.mailTextBox.Size = new System.Drawing.Size(355, 27);
-            this.mailTextBox.TabIndex = 14;
             // 
             // userNameLabel
             // 
@@ -102,6 +83,26 @@
             this.userNameTextBox.Size = new System.Drawing.Size(355, 27);
             this.userNameTextBox.TabIndex = 13;
             // 
+            // mailLabel
+            // 
+            this.mailLabel.AutoSize = true;
+            this.mailLabel.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.mailLabel.Location = new System.Drawing.Point(6, 57);
+            this.mailLabel.Name = "mailLabel";
+            this.mailLabel.Size = new System.Drawing.Size(99, 17);
+            this.mailLabel.TabIndex = 15;
+            this.mailLabel.Text = "電子郵件地址：";
+            // 
+            // mailTextBox
+            // 
+            this.mailTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mailTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.mailTextBox.Location = new System.Drawing.Point(141, 54);
+            this.mailTextBox.Name = "mailTextBox";
+            this.mailTextBox.Size = new System.Drawing.Size(355, 27);
+            this.mailTextBox.TabIndex = 14;
+            // 
             // serverInfoGroupBox
             // 
             this.serverInfoGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
@@ -118,16 +119,25 @@
             this.serverInfoGroupBox.TabStop = false;
             this.serverInfoGroupBox.Text = "伺服器資訊";
             // 
-            // enableSSLCheckBox
+            // SmtpServerLabel
             // 
-            this.enableSSLCheckBox.AutoSize = true;
-            this.enableSSLCheckBox.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.enableSSLCheckBox.Location = new System.Drawing.Point(141, 54);
-            this.enableSSLCheckBox.Name = "enableSSLCheckBox";
-            this.enableSSLCheckBox.Size = new System.Drawing.Size(74, 21);
-            this.enableSSLCheckBox.TabIndex = 19;
-            this.enableSSLCheckBox.Text = "啟用SSL";
-            this.enableSSLCheckBox.UseVisualStyleBackColor = true;
+            this.SmtpServerLabel.AutoSize = true;
+            this.SmtpServerLabel.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.SmtpServerLabel.Location = new System.Drawing.Point(6, 24);
+            this.SmtpServerLabel.Name = "SmtpServerLabel";
+            this.SmtpServerLabel.Size = new System.Drawing.Size(129, 17);
+            this.SmtpServerLabel.TabIndex = 16;
+            this.SmtpServerLabel.Text = "外送伺服器(SMTP)：";
+            // 
+            // SmtpServerTextBox
+            // 
+            this.SmtpServerTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SmtpServerTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.SmtpServerTextBox.Location = new System.Drawing.Point(141, 21);
+            this.SmtpServerTextBox.Name = "SmtpServerTextBox";
+            this.SmtpServerTextBox.Size = new System.Drawing.Size(234, 27);
+            this.SmtpServerTextBox.TabIndex = 16;
             // 
             // serverPortLabel
             // 
@@ -151,25 +161,16 @@
             this.serverPortTextBox.Text = "25";
             this.serverPortTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // SmtpServerTextBox
+            // enableSSLCheckBox
             // 
-            this.SmtpServerTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.SmtpServerTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.SmtpServerTextBox.Location = new System.Drawing.Point(141, 21);
-            this.SmtpServerTextBox.Name = "SmtpServerTextBox";
-            this.SmtpServerTextBox.Size = new System.Drawing.Size(234, 27);
-            this.SmtpServerTextBox.TabIndex = 16;
-            // 
-            // SmtpServerLabel
-            // 
-            this.SmtpServerLabel.AutoSize = true;
-            this.SmtpServerLabel.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.SmtpServerLabel.Location = new System.Drawing.Point(6, 24);
-            this.SmtpServerLabel.Name = "SmtpServerLabel";
-            this.SmtpServerLabel.Size = new System.Drawing.Size(129, 17);
-            this.SmtpServerLabel.TabIndex = 16;
-            this.SmtpServerLabel.Text = "外送伺服器(SMTP)：";
+            this.enableSSLCheckBox.AutoSize = true;
+            this.enableSSLCheckBox.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.enableSSLCheckBox.Location = new System.Drawing.Point(141, 54);
+            this.enableSSLCheckBox.Name = "enableSSLCheckBox";
+            this.enableSSLCheckBox.Size = new System.Drawing.Size(74, 21);
+            this.enableSSLCheckBox.TabIndex = 19;
+            this.enableSSLCheckBox.Text = "啟用SSL";
+            this.enableSSLCheckBox.UseVisualStyleBackColor = true;
             // 
             // loginInfoGroupBox
             // 
@@ -179,22 +180,13 @@
             this.loginInfoGroupBox.Controls.Add(this.loginUsernameTextBox);
             this.loginInfoGroupBox.Controls.Add(this.loginPasseordLabel);
             this.loginInfoGroupBox.Controls.Add(this.loginPasseordTextBox);
+            this.loginInfoGroupBox.Controls.Add(this.noSavePasswordCheckBox);
             this.loginInfoGroupBox.Location = new System.Drawing.Point(12, 204);
             this.loginInfoGroupBox.Name = "loginInfoGroupBox";
-            this.loginInfoGroupBox.Size = new System.Drawing.Size(502, 100);
+            this.loginInfoGroupBox.Size = new System.Drawing.Size(502, 116);
             this.loginInfoGroupBox.TabIndex = 2;
             this.loginInfoGroupBox.TabStop = false;
             this.loginInfoGroupBox.Text = "登入資訊";
-            // 
-            // loginPasseordLabel
-            // 
-            this.loginPasseordLabel.AutoSize = true;
-            this.loginPasseordLabel.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.loginPasseordLabel.Location = new System.Drawing.Point(6, 57);
-            this.loginPasseordLabel.Name = "loginPasseordLabel";
-            this.loginPasseordLabel.Size = new System.Drawing.Size(47, 17);
-            this.loginPasseordLabel.TabIndex = 15;
-            this.loginPasseordLabel.Text = "密碼：";
             // 
             // loginUsernameLabel
             // 
@@ -205,6 +197,26 @@
             this.loginUsernameLabel.Size = new System.Drawing.Size(86, 17);
             this.loginUsernameLabel.TabIndex = 15;
             this.loginUsernameLabel.Text = "使用者名稱：";
+            // 
+            // loginUsernameTextBox
+            // 
+            this.loginUsernameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loginUsernameTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.loginUsernameTextBox.Location = new System.Drawing.Point(141, 21);
+            this.loginUsernameTextBox.Name = "loginUsernameTextBox";
+            this.loginUsernameTextBox.Size = new System.Drawing.Size(355, 27);
+            this.loginUsernameTextBox.TabIndex = 14;
+            // 
+            // loginPasseordLabel
+            // 
+            this.loginPasseordLabel.AutoSize = true;
+            this.loginPasseordLabel.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.loginPasseordLabel.Location = new System.Drawing.Point(6, 57);
+            this.loginPasseordLabel.Name = "loginPasseordLabel";
+            this.loginPasseordLabel.Size = new System.Drawing.Size(47, 17);
+            this.loginPasseordLabel.TabIndex = 15;
+            this.loginPasseordLabel.Text = "密碼：";
             // 
             // loginPasseordTextBox
             // 
@@ -217,21 +229,11 @@
             this.loginPasseordTextBox.TabIndex = 14;
             this.loginPasseordTextBox.UseSystemPasswordChar = true;
             // 
-            // loginUsernameTextBox
-            // 
-            this.loginUsernameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.loginUsernameTextBox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.loginUsernameTextBox.Location = new System.Drawing.Point(141, 21);
-            this.loginUsernameTextBox.Name = "loginUsernameTextBox";
-            this.loginUsernameTextBox.Size = new System.Drawing.Size(355, 27);
-            this.loginUsernameTextBox.TabIndex = 14;
-            // 
             // OKButton
             // 
             this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.OKButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.OKButton.Location = new System.Drawing.Point(358, 314);
+            this.OKButton.Location = new System.Drawing.Point(358, 330);
             this.OKButton.Name = "OKButton";
             this.OKButton.Size = new System.Drawing.Size(75, 23);
             this.OKButton.TabIndex = 3;
@@ -242,7 +244,7 @@
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(439, 314);
+            this.cancelButton.Location = new System.Drawing.Point(439, 330);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 4;
@@ -251,12 +253,24 @@
             // 
             // sendTestMailButton
             // 
-            this.sendTestMailButton.Location = new System.Drawing.Point(12, 314);
+            this.sendTestMailButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.sendTestMailButton.Location = new System.Drawing.Point(12, 330);
             this.sendTestMailButton.Name = "sendTestMailButton";
             this.sendTestMailButton.Size = new System.Drawing.Size(75, 23);
             this.sendTestMailButton.TabIndex = 5;
             this.sendTestMailButton.Text = "寄測試信";
             this.sendTestMailButton.UseVisualStyleBackColor = true;
+            // 
+            // noSavePasswordCheckBox
+            // 
+            this.noSavePasswordCheckBox.AutoSize = true;
+            this.noSavePasswordCheckBox.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.noSavePasswordCheckBox.Location = new System.Drawing.Point(141, 87);
+            this.noSavePasswordCheckBox.Name = "noSavePasswordCheckBox";
+            this.noSavePasswordCheckBox.Size = new System.Drawing.Size(131, 21);
+            this.noSavePasswordCheckBox.TabIndex = 19;
+            this.noSavePasswordCheckBox.Text = "寄信前在詢問密碼";
+            this.noSavePasswordCheckBox.UseVisualStyleBackColor = true;
             // 
             // EmailSettings
             // 
@@ -264,7 +278,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(526, 347);
+            this.ClientSize = new System.Drawing.Size(526, 363);
             this.Controls.Add(this.userInfoGroupBox);
             this.Controls.Add(this.serverInfoGroupBox);
             this.Controls.Add(this.loginInfoGroupBox);
@@ -307,5 +321,6 @@
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.CheckBox enableSSLCheckBox;
         private System.Windows.Forms.Button sendTestMailButton;
+        private System.Windows.Forms.CheckBox noSavePasswordCheckBox;
     }
 }
