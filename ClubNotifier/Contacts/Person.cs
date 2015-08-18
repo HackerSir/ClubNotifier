4cfc68f937476f13d90fb517254d69891747865d
Chan, Danny <danny50610@yahoo.com.tw>
2015-08-18 19:33:13 +0800

    Add basic Person Class

---
+++ ClubNotifier/Contacts/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubNotifier.Contacts {
+    class Person {
+        //聯絡人姓名
+        public String Name { get; set; }
+        //電子郵件
+        public MailAddress eMail;
+    }
+}
