889b30864fb188b243e90d528c629c1a4a11a10c
Chan, Danny <danny50610@yahoo.com.tw>
2015-08-31 13:23:44 +0800

    Enable checkbox in datagridview

--- ClubNotifier/ContactSelectForm.Designer.cs
+++ ClubNotifier/ContactSelectForm.Designer.cs
@@ -50,7 +50,6 @@
             this.JobColumn});
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(561, 312);
             this.dataGridView1.TabIndex = 0;
@@ -80,7 +79,6 @@
             this.SelectedColumn.Frozen = true;
             this.SelectedColumn.HeaderText = "選擇";
             this.SelectedColumn.Name = "SelectedColumn";
-            this.SelectedColumn.ReadOnly = true;
             this.SelectedColumn.Width = 50;
             // 
             // NameColumn
